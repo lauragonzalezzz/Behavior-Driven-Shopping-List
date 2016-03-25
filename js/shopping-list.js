6e5b83881718b8490f6a9c7a4993b6e9cb797049
Laura <lg3295@gmail.com>
2016-03-25 12:31:36 -1000

    tests functionality of adItem

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -2,6 +2,14 @@ var ShoppingList = function(){
   this.items = [];
 
   this.addItem = function(item){
+  //   if (item !== instanceOf ShoppingListItem) {
+  //     throw.new Error('Not a shopping list item');
+  // }
     return this.items.push(item);
   };
 };
+
+var SL = new ShoppingList();
+console.log(this.items);
+SL.addItem('hello');
+console.log('this.items',this.items);
