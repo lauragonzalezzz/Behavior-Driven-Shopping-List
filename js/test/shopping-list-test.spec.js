fa3ee014e37034b71d39d8365abb28a7a9b682af
Laura <lg3295@gmail.com>
2016-03-24 16:36:28 -1000

    adds test for description property

--- js/test/shopping-list-test.spec.js
+++ js/test/shopping-list-test.spec.js
@@ -1,6 +1,9 @@
 
 describe('ShoppingListItem', function() {
-
+  var SL = null;
+  beforeEach(function() {
+    SL = new ShoppingListItem();
+  });
 
 	it ('should be a function', function() {
 		expect(ShoppingListItem).to.be.a('function');
@@ -9,10 +12,14 @@ describe('ShoppingListItem', function() {
 
 // NAME
 	it('should have a name property', function() {
-		var SL = new ShoppingListItem();
 		expect(SL).to.have.property('name');
-		var SL2 = new ShoppingListItem('happy');
-		expect(SL2.name).to.equal('happy');
 	});
+
+// DESCRIPTION
+	it('should have a description property', function() {
+		expect(SL).to.have.property('description');
+	});
+
+
 
 });
