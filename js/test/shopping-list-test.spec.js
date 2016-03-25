acd4e752c371ee2e3c2115500f0c94c110fe8427
Laura <lg3295@gmail.com>
2016-03-25 11:45:30 -1000

    tests that addItem pushes arguments into items array

--- js/test/shopping-list-test.spec.js
+++ js/test/shopping-list-test.spec.js
@@ -21,13 +21,26 @@ describe('ShoppingList', function(){
 
   describe('Methods', function(){
 
-    it('should have an addItem method', function(){
-      expect(SL.addItem).to.exist;
+    describe('addItem', function(){
+
+      it('should have an addItem method', function(){
+        expect(SL.addItem).to.exist;
+      });
+
+      it('addItem should be a function', function(){
+        expect(SL.addItem).to.be.a('function');
+      });
+
+      it('addItem should be a function', function(){
+        var SL1 = new ShoppingList();
+        SL1.addItem('grapes');
+        expect(SL1.items).to.deep.equal(['grapes']);
+      });
+
     });
 
-    it('addItem should be a function', function(){
-      expect(SL.addItem).to.be.a('function');
-    });
+
+
 
   });
 });
