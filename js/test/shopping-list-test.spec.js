cdae70c7e16b78aca2eed83ea282d885a33d2077
Laura <lg3295@gmail.com>
2016-03-25 11:28:29 -1000

    test if shopping list has an items property

--- js/test/shopping-list-test.spec.js
+++ js/test/shopping-list-test.spec.js
@@ -1,7 +1,18 @@
 describe('ShoppingList', function(){
 
+  var SL = null;
+
+  beforeEach(function() {
+    SL = new ShoppingList();
+  });
+
+
   it('should be a function', function(){
     expect(ShoppingList).to.be.a('function');
+  });
+
+  it('should have an items property', function(){
+    expect(SL).to.have.property('items');
   });
 
 });
