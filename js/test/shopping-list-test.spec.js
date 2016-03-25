a1050157e8978d0b4fba5dfd4b24184b1640bd77
Laura <lg3295@gmail.com>
2016-03-24 16:09:16 -1000

    checks name property

--- js/test/shopping-list-test.spec.js
+++ js/test/shopping-list-test.spec.js
@@ -1,10 +1,18 @@
 
-describe('Shopping List', function() {
+describe('ShoppingListItem', function() {
+
 
 	it ('should be a function', function() {
-		expect('ShoppingList').to.be.a('function');
+		expect(ShoppingListItem).to.be.a('function');
 	});
 
 
+// NAME
+	it('should have a name property', function() {
+		var SL = new ShoppingListItem();
+		expect(SL).to.have.property('name');
+		var SL2 = new ShoppingListItem('happy');
+		expect(SL2.name).to.equal('happy');
+	});
 
 });
