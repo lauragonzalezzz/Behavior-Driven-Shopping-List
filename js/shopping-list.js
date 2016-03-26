045387743b4e20e7b06e2bf3b754fe6106fa1de9
Laura <lg3295@gmail.com>
2016-03-25 15:55:09 -1000

    adds remove item method functionality

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,12 +1,27 @@
 var ShoppingList = function(){
+
   this.items = [];
+
 
   this.addItem = function(item){
     if (item instanceof ShoppingListItem === false) {
-      throw new Error('Not a shopping list item');
+      throw new Error('Not a shopping list item.');
     }
     return this.items.push(item);
   };
 
-  this.removeItem = function(){};
+  this.removeItem = function(item){
+    if (!item && this.items.length > 0) {
+      this.items.pop();
+      return;
+    }
+    var index = this.items.indexOf(item);
+    console.log('index',index);
+    if (index === -1) {
+      throw new Error('Not in the shopping list.');
+    }
+    else {
+      this.items.splice(index, 1);
+    };
+  };
 };
