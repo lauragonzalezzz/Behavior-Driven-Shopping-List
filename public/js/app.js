bea41f7b29242b0eaa5779bd63c857acdac95499
Laura <lg3295@gmail.com>
2016-03-28 11:34:04 -1000

    reverts to checkbox being added dynamically

--- public/js/app.js
+++ public/js/app.js
@@ -8,15 +8,19 @@ var idx = 0;
 function add_to_shopping_list() {
   var title = document.getElementById('title').value;
   var description = ' ' + document.getElementById('description').value;
-  var checkbox = document.getElementById('checkbox');
+  var checkbox = document.createElement('input');
+  checkbox.id = title;
+  checkbox.type="checkbox";
+  checkbox.addEventListener('onchange', changeCheckedStatus(idx, checkbox));
+
   var new_shopping_list_item = new ShoppingListItem(title, description);
 
-  checkbox.addEventListener('onchange', changeCheckedStatus(idx, checkbox));
   myShoppingList.addItem(new_shopping_list_item);
 
   var myList = myShoppingList.render();
   var contentDiv = document.getElementById('content')
-  contentDiv.innerHTML = checkbox + ' ' + myList;
+  contentDiv.innerHTML = myList;
+  contentDiv.appendChild(checkbox);
   return;
 };
 
